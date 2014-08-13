32d4ae16c8fbf7c3c1c9f4a3e6b0b68dfeaa662b
yejincho <joyajin91@gmail.com>
2014-08-13 22:51:29 +0900

    metro-url 전송 테스트2

--- Extension/js/background.js
+++ Extension/js/background.js
@@ -1,9 +1,8 @@
-chrome.tabs.getCurrent(null,function(tab) {
-		var url='http://localhost:8080/background';
-		$.getJSON(url, function(object){
-			if(object.code==200){
-				
-			}
+chrome.tabs.getCurrent( function(tab) {
+	var url = 'http://localhost:8080/background';
+	$.getJSON(url, function(object) {
+		if (object.code == 200) {
+
 		}
 	});
-}
+});
