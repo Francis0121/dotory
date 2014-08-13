f89327d5fc3e7fd04a1504018a07908753576461
yejincho <joyajin91@gmail.com>
2014-08-14 00:18:26 +0900

    metro-query

--- Extension/js/background.js
+++ Extension/js/background.js
@@ -1,8 +1,17 @@
-chrome.tabs.getCurrent( function(tab) {
+/*chrome.tabs.getCurrent( function(tab) {
 	var url = 'http://localhost:8080/background';
 	$.getJSON(url, function(object) {
 		if (object.code == 200) {
-
+			alert(tab);
+		}
+	});
+});*/
+// { currentWindow: true, active: true }
+chrome.tabs.query({lastFocusedWindow: true, active: true} ,function (tabs) {
+	var url = 'http://localhost:8080/background';
+	$.getJSON(url, function(object) {
+		if (object.code == 200) {
+			alert(tabs[0].url);
 		}
 	});
 });
