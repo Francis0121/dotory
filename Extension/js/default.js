c36036f50cc7a8471c8b7106809a9dfd56094cd1
Francis Kim <kims_0121@naver.com>
2014-08-17 22:47:03 +0900

    Extension에 변경.

---
+++ Extension/js/default.js
@@ -0,0 +1,15 @@
+if(typeof dotory == 'undefined'){
+	dotory = {};
+}
+
+dotory.contextPath = 'http://localhost:8080';
+dotory.types = {
+	main : 'MAIN',
+	background : 'BACKGROUND'
+};
+
+
+dotory.regexCategories = {
+	image : 1,
+	title : 2
+};
