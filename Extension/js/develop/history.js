66e607074d20c623300f68e2d66dd5447d676668
Francis Kim <kims_0121@naver.com>
2014-08-24 20:35:06 +0900

    자.

--- Extension/js/develop/history.js
+++ Extension/js/develop/history.js
@@ -24,6 +24,7 @@ dotory.history.binding = function(){
 	// style testing sample data ~ TODO delete
 //	dotory.history.testing();
 	dotory.history.loading();
+	dotory.history.total();
 };
 
 dotory.history.testing = function(){
@@ -42,6 +43,61 @@ dotory.history.testing = function(){
 		parent.append(li.clone());
 	}
 };
+
+dotory.history.total = function(){
+	var url = dotory.contextPath + '/history/total';
+		json = { userPn : dotory.user.pn };
+
+	$.getJSON(url, json, function(object){
+		var data = object.data;
+		
+		if(data.code == 200){
+			var keywords = data.keywords,
+				dates = data.dates,
+				keywordArray = new Array();
+			
+			var pageWrap = $('.opened_page_list_wrap'),
+				dateWrap = $('.history_list_wrap'),
+				keywordTotal = $('.page_head .keyword_total');
+				
+			var pageHtml = $(pageWrap.html()), 
+				dateHtml = $(dateWrap.html());
+
+			// keywords
+			for (var i=0; i < keywords.length; i++) {
+				var keyword = keywords[i];
+
+				if(keywordArray.indexOf(keyword.keyword) == -1){
+					keywordArray.push(keyword.keyword);
+				}
+				
+				var $object = pageHtml.find('.opend_page_link');
+				$object.attr('href', keyword.url);
+				$object.text(title);
+				
+				$object = pageHtml.find('.opened_page_check');
+				$object.attr('value', keyword.keyword);
+
+				pageWrap.append(pageHtml);					
+			}
+			// ~ keyword array 
+			var strKeywordHtml = '';
+			for(var i=0; i<keywordTotal; i++){
+				var strKeyword = keywordTotal[i];
+				strKeywordHtml += (strKeyword + ' ')
+			}
+			keywordTotal(strKeywordHtml);
+			// ~ dates
+			for (var i=0; i < dates.lenght; i++){
+			
+				var date = dates[i];
+			}
+		}
+		
+	});
+	
+};
+
 
 dotory.history.loading=function(){
 	var url=dotory.contextPath+'/history/keyword',
